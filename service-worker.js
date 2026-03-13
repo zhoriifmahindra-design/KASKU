0a8bc62305278e85c2de3065d38951493f4e1ba7
zhoriifmahindra-design <zhoriifmahindra@gmail.com>
2026-03-14 03:06:56 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,32 @@
-self.addEventListener("install", function(e) {
-console.log("Kas Manager Installed");
+const CACHE_NAME = "kasku-v1";
+
+const urlsToCache = [
+"./",
+"./index.html",
+"./style.css",
+"./app.js",
+"./manifest.json",
+"./img_iconkas.png"
+];
+
+self.addEventListener("install", function(event){
+
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(function(cache){
+return cache.addAll(urlsToCache);
+})
+);
+
+});
+
+self.addEventListener("fetch", function(event){
+
+event.respondWith(
+caches.match(event.request)
+.then(function(response){
+return response || fetch(event.request);
+})
+);
+
 });
